aa959d3de43f9b8249dda80d75f9d032fae6c7f2
NickDoublet <39459905+NickDoublet@users.noreply.github.com>
2022-04-20 12:40:57 +1000

    Added Banner, visuals complete

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 		<>
 			{/* <NewGameMenu /> */}
 			<Game />
-			{/* <Banner /> */}
+			<Banner />
 		</>
 	)
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,24 @@
+import yellowO64 from '../assets/icon-o.svg'
+
 const Banner = () => {
-	return <div>Banner</div>
+	return (
+		<>
+			<div className='bg-darken'></div>
+			<div className='banner'>
+				<div>
+					<p>OH NO, YOU LOST...</p>
+				</div>
+				<div>
+					<img src={yellowO64} alt='' />
+					<h1>TAKES THE ROUND</h1>
+				</div>
+				<div>
+					<button className='btn-quit'>QUIT</button>
+					<button className='btn-nextrnd'>NEXT ROUND</button>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Banner
